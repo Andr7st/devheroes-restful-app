f94e14a68b9c1660d174b605b9feb5bc34720fd3
Andrés Segura <andres.segura.dev@gmail.com>
2023-08-06 21:06:12 -0500

    initial commit: Restful API project for DevHeroes Team

---
+++ src/README.markdown
@@ -0,0 +1,2 @@
+## DevHeroes App
+
---
+++ src/test/java/com/devheroes/restful/app/RestfulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devheroes.restful.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
